cd5f537edb34c5a748ca2b4c5eee979ed19ef96b
Davis Batten <davisbatten@Daviss-MacBook-Pro.local>
2016-08-14 19:15:00 -0500

    moved semantic to public/lib, added Book model

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+//MongoDB Model Setup
+var mongoose = require('mongoose');
+require('./models/Books');
+mongoose.connect('mongodb://localhost/bookworm');
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
---
+++ models/Books.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var BookSchema = new mongoose.Schema ({
+    title: String,
+    author: String,
+    description: String,
+    ISBN: String
+});
+
+mongoose.model('Book', BookSchema);
